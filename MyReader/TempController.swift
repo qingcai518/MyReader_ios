ea8fa8d436bf2b9482a5b8407e5d2a77064f5ef4
Qingcan <q_li@fullspeed.co.jp>
2017-02-24 15:26:54 +0900

    change to image

--- MyReader/TempController.swift
+++ MyReader/TempController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class TempController: LeavesViewController {
     
-//    let images = [ UIImage(named : "icon_book_pause")!,
-//                   UIImage(named: "icon_book_pull"),
-//                   UIImage(named: "icon_book1"),
-//                   UIImage(named: "icon_book2"),
-//                   UIImage(named: "icon_book3"),
-//                   UIImage(named: "icon_book4")]
+    let images = [ UIImage(named : "icon_book_pause")!,
+                   UIImage(named: "icon_book_pull")!,
+                   UIImage(named: "icon_book1")!,
+                   UIImage(named: "icon_book2")!,
+                   UIImage(named: "icon_book3")!,
+                   UIImage(named: "icon_book4")!]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,14 +27,21 @@ class TempController: LeavesViewController {
     
     // #program mark
     override func numberOfPagesInLeavesView(leavesView: LeavesView) -> Int {
-        return 3
+        return images.count
     }
     
     override func renderPageAtIndex(index: Int, inContext context: CGContext) {
-        print("11111")
+        let image = images[index]
+        let imageRect = CGRect(x: 0, y: 0, width: screenWidth
+            , height: screenHeight)
         
-        let bounds = context.boundingBoxOfClipPath
-        context.setFillColor(UIColor(hue: CGFloat(index) / 10.0, saturation: 0.8, brightness: 0.8, alpha: 1.0).cgColor)
-        context.fill(bounds.insetBy(dx: 100, dy: 100))
+        let transform = AppUtility.aspectFit(innerRect: imageRect, outerRect: context.boundingBoxOfClipPath)
+        context.concatenate(transform)
+        
+        guard let cgImage = image.cgImage else {
+            return
+        }
+        
+        context.draw(cgImage, in: imageRect)
     }
 }
