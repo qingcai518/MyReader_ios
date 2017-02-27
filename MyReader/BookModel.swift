c285028e51bf676e68717f5720d8c2f0b94bc71a
Qingcan <q_li@fullspeed.co.jp>
2017-02-27 14:06:33 +0900

    paging

--- MyReader/BookModel.swift
+++ MyReader/BookModel.swift
@@ -9,8 +9,28 @@
 import Foundation
 
 class BookModel {
-    func readFile(bookInfo: LocalBookInfo, completion: @escaping (String?) -> Void) {
-        let encode = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.GB_18030_2000.rawValue))
+//    func readFile(bookInfo: LocalBookInfo, completion: @escaping (String?) -> Void) {
+//        let encode = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.GB_18030_2000.rawValue))
+//
+//        DispatchQueue.global().async {
+//            let readHandle = FileHandle(forReadingAtPath: bookInfo.localPath)
+//            guard let data = readHandle?.readData(ofLength: readLength) else {
+//                print("read no data.")
+//                return completion(nil)
+//            }
+//            
+//            let readStr = NSString.init(data: data, encoding: encode)
+//            
+//            DispatchQueue.main.async {
+//                print("read string = \(readStr)")
+//                return completion(readStr as? String)
+//            }
+//        }
+//    }
+    
+    func readFile(bookInfo: LocalBookInfo, completion : @escaping (String?) -> Void) {
+         let encode = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.macChineseSimp.rawValue))
+//        let encode = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.GB_2312_80.rawValue))
         
         DispatchQueue.global().async {
             let readHandle = FileHandle(forReadingAtPath: bookInfo.localPath)
@@ -21,10 +41,8 @@ class BookModel {
             
             let readStr = String.init(data: data, encoding: String.Encoding(rawValue: encode))
             
-            // 指定されたエンコードでファイルの読み込みを実施する.
             DispatchQueue.main.async {
-                 print("read str = \(readStr)")
-                
+                print("read string = \(readStr)")
                 return completion(readStr)
             }
         }
