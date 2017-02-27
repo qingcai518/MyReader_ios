1f13d9d515018269889b9fe48ccaa2ef0faf6539
Qingcan <q_li@fullspeed.co.jp>
2017-02-27 11:37:07 +0900

    size of read file

--- MyReader/AppConstants.swift
+++ MyReader/AppConstants.swift
@@ -18,7 +18,7 @@ let textHeight = screenHeight - UIApplication.shared.statusBarFrame.size.height 
 let baseUrl = "https://main-myreader.ssl-lolipop.jp"
 let bookService = baseUrl + "/Books"
 
-let readLength = 1024 * 10
+let readLength = 2048
 
 struct NotificationName {
     static let FinishDownload = "FinishDownload"
