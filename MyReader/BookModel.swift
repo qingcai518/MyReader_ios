34eb9c6ffb4c0d67667ce18ba9ab45741be2b83d
Qingcan <q_li@fullspeed.co.jp>
2017-02-27 14:21:07 +0900

    paging

--- MyReader/BookModel.swift
+++ MyReader/BookModel.swift
@@ -9,26 +9,6 @@
 import Foundation
 
 class BookModel {
-//    func readFile(bookInfo: LocalBookInfo, completion: @escaping (String?) -> Void) {
-//        let encode = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.GB_18030_2000.rawValue))
-//
-//        DispatchQueue.global().async {
-//            let readHandle = FileHandle(forReadingAtPath: bookInfo.localPath)
-//            guard let data = readHandle?.readData(ofLength: readLength) else {
-//                print("read no data.")
-//                return completion(nil)
-//            }
-//            
-//            let readStr = NSString.init(data: data, encoding: encode)
-//            
-//            DispatchQueue.main.async {
-//                print("read string = \(readStr)")
-//                return completion(readStr as? String)
-//            }
-//        }
-//    }
-    
-    
     func readFile(bookInfo: LocalBookInfo, completion : @escaping (String?) -> Void) {
          let encode = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.macChineseSimp.rawValue))
 //        let encode = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.GB_2312_80.rawValue))
