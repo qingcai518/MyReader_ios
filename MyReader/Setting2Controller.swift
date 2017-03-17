ade6be177e73c0b9ca226d5cd28853872618b83d
Qingcan <q_li@fullspeed.co.jp>
2017-03-17 18:14:19 +0900

    next line

--- MyReader/Setting2Controller.swift
+++ MyReader/Setting2Controller.swift
@@ -52,7 +52,6 @@ class Setting2Controller: UIViewController {
         smallBtn.isEnabled = true
         if (fontSize >= 30) {
             bigBtn.isEnabled = false
-            
         }
         
     }
